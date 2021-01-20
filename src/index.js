3d1098c4b362181bf5de64cfcdf44c71681f4452
LiubovShpakova <68159626+LiubovShpakova@users.noreply.github.com>
2021-01-20 18:12:28 +0200

    add markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import menuItems from './templates/menu-item.hbs';
+import menu from './menu.json';
 import './styles.css';
+
+const gallery = document.querySelector('ul.js-menu');
+const markup = menuItems(menu);
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
