1f2d2ab1970142b4d69bfec81198959df9fe3778
LiubovShpakova <68159626+LiubovShpakova@users.noreply.github.com>
2021-01-24 14:29:55 +0200

    add theme

--- src/index.js
+++ src/index.js
@@ -10,3 +10,31 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+const bodyRef = document.querySelector('body');
+const themeRefs = document.querySelector('.theme-switch__toggle');
+
+firstColor();
+function firstColor() {
+  if (localStorage.getItem('theme') === 'dark-theme') {
+    themeRefs.checked = true;
+    bodyRef.classList.add((localStorage.theme = Theme.DARK));
+  } else {
+    themeRefs.checked = false;
+  }
+}
+
+themeRefs.addEventListener('change', changeTheme);
+function changeTheme(event) {
+  if (event.currentTarget.checked) {
+    bodyRef.classList.remove((localStorage.theme = Theme.LIGHT));
+    bodyRef.classList.add((localStorage.theme = Theme.DARK));
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', 'dark-theme');
+    return;
+  }
+  bodyRef.classList.add((localStorage.theme = Theme.LIGHT));
+  bodyRef.classList.remove((localStorage.theme = Theme.DARK));
+  localStorage.removeItem('theme');
+  localStorage.setItem('theme', 'light-theme');
+  return;
+}
